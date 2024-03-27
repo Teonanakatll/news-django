faea477a8606a6330292fe10f54debd70638bbcf
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-03-27 19:26:10 +0300

    решаю с шерингом

--- news/settings_prod.py
+++ news/settings_prod.py
@@ -12,27 +12,3 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-
-    'news_app.apps.NewsAppConfig',
-    # https://django-extensions.readthedocs.io/en/latest/ документация по расширениям джанго
-    'django_extensions',
-    'debug_toolbar',
-
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
-    'metatags',
-    'django_ckeditor_5',
-    'taggit',
-    'sorl.thumbnail',
-
-]
